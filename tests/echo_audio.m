5934b7306afd6ff5a9e0e22756e6d9faec337a1c
Daniel Pineda <daniel-p96@outlook.com>
2021-05-26 16:44:44 -0600

    Efectos aplican a 1 o 2 canales segun el caso. Efectos usan el resultado del pasabandas. Codigo mas ordenado y limpio.

--- tests/echo_audio.m
+++ tests/echo_audio.m
@@ -7,24 +7,33 @@ test_signal = sawtooth(2*pi*5*t);
 test_signal(test_signal < 0)=0;
 test_signal(10000:end)=0;
 
-x = test_signal;
+[songData, Fs] = audioread("../audio_sample_dual_channel.wav");
 
-y = echo_audio(x, 0.5, 0.1, Fs);
+%x = test_signal;
+x = songData;
+
+if (size(x, 2) == 2)
+    y(:,1) = echo_effect(x(:,1), 0.5, 0.1, Fs);
+    y(:,2) = echo_effect(x(:,2), 0.5, 0.1, Fs);
+else
+    y = echo_effect(x, 0.5, 0.1, Fs);    
+end
+
 
 % se escucha "hola" con varios ecos
 
 figure(2);
 subplot(2,1,1);
-plot(t1, x);
+plot(x);
 title('x');
 xlabel('time (s)');
 
 subplot(2,1,2);
-plot(t1,y);
+plot(y);
 title('y');
 xlabel('time (s)');
 
-function y = echo_audio(x, alfa, delay, Fs)
+function y = echo_effect(x, alfa, delay, Fs)
 
     y = zeros(size(x));
     y_n = 0;
